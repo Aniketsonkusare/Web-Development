10f0508ab0be21c0459e666c281cfe2e9b8f5a51
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-11-22 22:28:33 +0530

    Alert-Confirm-Prompt

---
+++ 54 Alert-Confirm-Prompt/script.js
@@ -0,0 +1,20 @@
+// alert("this is a alert box")
+
+
+console.log("this is a console...");
+console.log("this is also console...");
+
+var a = prompt("Enter Your Age");
+console.log("Your age is " + a);
+
+var istrue = confirm("are you confirm you age is " + a);
+if (istrue) {
+    console.log("yes i am confirm");
+}
+else{
+    console.log("no i am not sure");
+}
+
+document.title = "good"
+
+document.body.style.backgroundColor = "red"
