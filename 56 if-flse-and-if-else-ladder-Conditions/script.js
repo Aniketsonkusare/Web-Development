683ad1ecd91cef9dda989bf08e0dd2efd91ea286
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-11-26 21:48:26 +0530

    if-else-and-if-else-ladder

---
+++ 56 if-flse-and-if-else-ladder-Conditions/script.js
@@ -0,0 +1,63 @@
+console.log("if-else condition");
+
+// if-else condition
+let age = 200;
+if (age == 200) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+if (age > 120) {
+    console.log("condition is false");
+}
+else{
+    console.log("conditopn is true");
+}
+
+let age1 = 18
+
+if (age === 17) {
+    console.log("you cannot drive, wait 1 year");
+}
+else if (age < 15) {
+    console.log("you cannot drive, wait 3 years");
+}
+
+else if (age < 16) {
+    console.log("you cannot drive, wait 2 years");
+}
+else if (age > 18) {
+    console.log("you can drive");
+}
+
+else{
+    console.log("this is other condition applied");
+}
+
+let age3 = 18;
+
+if (age3 != 18) {
+    console.log("you can drive");
+}
+else if (age3 == 0) {
+    console.log("Are You Kidding");
+}
+else{
+    console.log("you can not drive");
+}
+
+a = 10;
+b = 12;
+let c = a > b ? (a - b) : (b - a);
+console.log(c);
+
+// single comment
+
+/*
+This is a 
+Multiline 
+Comment
+*/
+
