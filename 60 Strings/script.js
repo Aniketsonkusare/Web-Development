9302fce381a4d64c8b21491a9076baaa17da6e22
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-11-27 13:33:12 +0530

    Strings

---
+++ 60 Strings/script.js
@@ -0,0 +1,26 @@
+console.log("this is a string");
+
+let a = "Aniket";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+
+let real_name = "A";
+let brother_name = "B"
+
+console.log("my name is " + real_name + " and my brother name is " + brother_name);
+console.log(`my name is ${real_name} and My brother name is ${brother_name}`);
+
+let b = "Aniket"
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,4));
+
+console.log(b.replace("Aniket","Harsh"));
+console.log(b.concat(a, "aniket","harsh"));
