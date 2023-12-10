23544c59aaa248f263d66e08aa1392317d967c56
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-12-10 22:47:54 +0530

    DOM

---
+++ 66 DOM/script.js
@@ -0,0 +1,6 @@
+document.title = "DOM"
+console.log(document.title);
+console.log(document.body);
+document.write("This is a Dom")
+document.body.style.background = "black"
+document.body.style.color = "white"
